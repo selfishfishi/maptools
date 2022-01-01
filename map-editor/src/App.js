79a00b8bb863644b40e55cd42c0dbbef57e80fab
Ali Fathalian <alifathalian@Alis-MacBook-Pro-2.local>
2022-01-01 18:20:58 -0500

    component: Added editor component

--- map-editor/src/App.js
+++ map-editor/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import Map from './components/Map/Map.js'
 import './App.css';
+import PropertyEditor from './components/ProperyEditor/PropertyEditor';
 
 function App() {
   const map_data = require('./files/output/inventory.json')
   return (
     <div className="App">
       <Map data={map_data}></Map>
+      <PropertyEditor className="Editor"></PropertyEditor>
     </div>
   );
 }
---
+++ map-editor/src/components/ProperyEditor/PropertyEditor.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './PropertyEditor.css'
+
+class PropertyEditor extends React.Component {
+    render() {
+        return ( 
+            <div className="Editor"> Hi</div> 
+        );
+    }
+
+}
+
+export default PropertyEditor;
