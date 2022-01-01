604494ba7e073eb1ef8c46892bf4f812fcb4f156
Ali Fathalian <alifathalian@Alis-MacBook-Pro-2.local>
2022-01-01 16:35:48 -0500

    editor: add map rows in separate divs

--- map-editor/src/components/Map/Map.js
+++ map-editor/src/components/Map/Map.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class Map extends React.Component {
     get_row_indexes(){
-        return 1
+        return 2
     }
     render_row(row_number){
 
@@ -11,22 +11,19 @@ class Map extends React.Component {
         for (let col in this.props.data) {
             let row_info = this.props.data[col][row_number];
             let image_src =  require('./../../files/' + row_info.path)
-            // let image_element = (<p>{image_src}</p>)
             let image_element = (<img key={row_info.id} src={image_src}></img>)
             row.push(image_element)
         }
         return row
-        // console.log(data_map.keys())
-        // for (let col of this.props.data.keys()){
-        //     console.log(col)
-        // } 
-        // return (<div key={row_number}>hi</div>)
     }
     render() {
         let row_count = this.get_row_indexes()
         let rows = [];
         for (let i = 0; i < row_count; i++) {
-            rows.push(this.render_row(i));
+            rows.push(<div key={i}>
+                {this.render_row(i)}
+            </div>
+                );
         }
         return ( 
             <div>{rows}</div> 
