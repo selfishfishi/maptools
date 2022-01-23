76831bc19b89a6abf901b7f3680e026b7e526bfc
Ali Fathalian <alifathalian@Alis-MacBook-Pro-2.local>
2022-01-22 19:17:30 -0500

    map_server: serve map inventory

--- map-editor/server/server.js
+++ map-editor/server/server.js
@@ -1,10 +1,34 @@
 const http = require("http");
+const fs = require("fs").promises;
+
+const port = 8080;
+const hostname = "127.0.0.1";
+const outputDir = "output";
 
 const requestListener = function (req, res) {
-  res.writeHead(200);
-  res.end("Hello, World!");
+  switch (req.url) {
+    case "/inventory":
+      getInventory(req, res);
+      break;
+    default:
+      res.writeHead(404);
+      res.end();
+  }
 };
+
+function getInventory(req, res) {
+  console.log("Getting inventory.json");
+  const filePath = __dirname + "/" + outputDir + "/inventory.json";
+  console.log(filePath);
+  fs.readFile(filePath).then((contents) => {
+    res.setHeader("Content-Type", "application/json");
+    res.writeHead(200);
+    res.end(contents);
+  });
+}
 
 const server = http.createServer(requestListener);
 console.log("Starting Map Server");
-server.listen(8080);
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
